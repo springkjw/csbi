11ee4494e6519e201f2921d44e3b21f5f5908695
KwonJaewon <springkjw@gmail.com>
2016-11-02 12:31:32 +0900

    image

--- protocol/views.py
+++ protocol/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import Protocol
+from .models import Protocol, PROTOCOL_TYPE
 
 
 def protocol_list(request):
     protocol = Protocol.objects.all()
+    type = PROTOCOL_TYPE
 
     template = 'protocol/protocol_list.html'
     context = {
-        "protocol": protocol
+        "protocol": protocol,
+        "type": type,
     }
 
     return render(request, template, context)
